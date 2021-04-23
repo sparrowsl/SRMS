441f5f17d1e35c9669bb233775897655f4462530
benjithorpe <benjaminthorpe19@gmail.com>
2021-04-23 20:04:14 +0000

    add condition to check for duplicate accounts

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import redirect
+from flask import url_for
+
 
 app = Flask(__name__)
 
+# accounts dictionary to store user's account
+accounts = {}
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-    return "Hello World of Flask!!!"
+    return render_template("index.html")
+
+@app.route('/signup')
+@app.route('/signup.html')
+def signup():
+    return render_template("signup.html")
 
 
-@app.route('/dash')
+@app.route('/dashboard')
 def dashboard():
-    accounts = {
-        "Medium"  : "medium",
-        "Facebook": "facebook",
-        "Google"  : "google",
-        "Twitter" : "twitter",
-        "Reddit"  : "reddit",
-        "Slack"   : "slack",
-        "Discord" : "discord",
-        "Telegram": "telegram"
-    }
+    accounts["Email"] = "email"
 
     username = request.args.get("name", "Nobody")
     print(f"Hello World, {username}")
@@ -31,12 +33,18 @@ def dashboard():
 
 
 @app.route('/add-new')
+@app.route('/add-account.html')
 def add_new_account():
     account_name = request.args.get("name", "John Doe")
     account_password = request.args.get("password", 5435)
-    return render_template("add-account.html", \
-                           name=account_name, \
-                           password=account_password)
+
+    # check if account is already present.
+    if not accounts.get(account_name):
+        accounts[account_name] = account_password
+    return redirect(url_for("dashboard"))
+    # render_template("add-account.html", \
+    #                        name=account_name, \
+    #                        password=account_password)
 
 
 if __name__ == "__main__":
