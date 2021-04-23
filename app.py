40ad21cbeced39baf424969637c6045d2e4a8f6c
benjithorpe <benjaminthorpe19@gmail.com>
2021-04-23 17:55:02 +0000

    Add dynamic display of accounts in the dashboard

--- app.py
+++ app.py
@@ -7,13 +7,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "Hello World of Flask!!!"
 
 
 @app.route('/dash')
 def dashboard():
-    name = request.args.get("name", "Nobody")
-    return render_template("dashboard.html", name=name)
+    accounts = {
+        "Medium"  : "medium",
+        "Facebook": "facebook",
+        "Google"  : "google",
+        "Twitter" : "twitter",
+        "Reddit"  : "reddit",
+        "Slack"   : "slack",
+        "Discord" : "discord",
+        "Telegram": "telegram"
+    }
+
+    username = request.args.get("name", "Nobody")
+    print(f"Hello World, {username}")
+    return render_template("dashboard.html", \
+                           username=username, \
+                           accounts=accounts)
+
+
+@app.route('/add-new')
+def add_new_account():
+    account_name = request.args.get("name", "John Doe")
+    account_password = request.args.get("password", 5435)
+    return render_template("add-account.html", \
+                           name=account_name, \
+                           password=account_password)
 
 
 if __name__ == "__main__":
