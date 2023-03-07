13e25adb44c50196344f69a3155f53a873dbd6bd
benjithorpe <benthorpe.dev@gmail.com>
2023-03-07 10:55:58 +0000

    add hooks file

---
+++ src/hooks.server.js
@@ -0,0 +1,25 @@
+import prisma from "./lib/utils/prisma.js";
+
+export const handle = async ({ resolve, event }) => {
+	const session = event.cookies.get("session");
+
+	if (!session) return await resolve(event);
+
+	const admin = await prisma.admin.findUnique({
+		where: {
+			id: session,
+		},
+		select: {
+			id: true,
+			username: true,
+			// email: true,
+			// dateJoined:true
+		},
+	});
+
+	console.log(admin);
+
+	if (admin) event.locals.admin = admin;
+
+	return await resolve(event);
+};
