69f053fbc472c38d4b4a2ecac53aa663f7abee0b
benjithorpe <benthorpe.dev@gmail.com>
2023-03-09 13:34:26 +0000

    add class model

--- src/lib/components/shared/TopNavigation.svelte
+++ src/lib/components/shared/TopNavigation.svelte
@@ -1,7 +1,11 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
 <nav class="bg-white flex shadow min-h-10vh px-5 top-0 sticky items-center justify-between">
 	<p>SRMS | Admin</p>
 
 	<form action="/logout" method="post">
-		<button>logout</button>
+		<Button classes="text-red-400 hover:text-red-600">logout</Button>
 	</form>
 </nav>
